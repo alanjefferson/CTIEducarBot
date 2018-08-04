0e1fe4a135bbf5b0b52c379602343ff57c7d5816
Alan Jefferson Nascimento <alan.jefferson@outlook.com>
2018-08-04 18:26:36 -0300

    Including project in repository

---
+++ CTIEducar/CosmoDB/CosmoDBConnector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+namespace CTIEducar.CosmoDB
+{
+	public class CosmoDBConnector
+	{
+		private const string EndpointUrl = "https://ajn.table.cosmosdb.azure.com:443/";
+		private const string PrimaryKey = "TLlNmk8dkjScFoAhimKoKDDnmpDoewaBLsvGQprBQvThOhJofdBKvqycHCG9BQGtynicusHbyLdIwCL8GGHTag==";
+		private DocumentClient client;
+
+		public async Task DataBaseConnector()
+		{
+			this.client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
+		}
+	}
+}
---
+++ CTIEducar/Dialogs/QnaDialog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+namespace CTIEducar.Dialogs
+{
+    [Serializable]
+    public class QnaDialog : QnAMakerDialog
+    {
+        public QnaDialog(bool convBegan = false) : base(new QnAMakerService( new QnAMakerAttribute(ConfigurationManager.AppSettings["QnaSubscriptionKey"], ConfigurationManager.AppSettings["QnaKnowledgebaseId"], "Não encontramos sua resposta. Por favor entre em contato pelo nosso WhatsApp: (11) 99335-6454. Estamos à disposição.", 0.5)))
+        {
+
+        }
+
+        private static void AddHeroCardToAnswer(Activity answer, string[] answerData)
+        {
+            var answerTitle = answerData[0];
+            var answerDesc = answerData[1];
+            var answerURL = answerData[2];
+            var answerImageURL = answerData[3];
+
+            HeroCard card = new HeroCard { Title = answerTitle, Subtitle = answerDesc, };
+            card.Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Visite nosso site.", value: answerURL) };
+            card.Images = new List<CardImage> { new CardImage(answerURL = answerImageURL, answerTitle) };
+            answer.Attachments.Add(card.ToAttachment());
+        }
+
+        protected override async Task RespondFromQnAMakerResultAsync(IDialogContext context, IMessageActivity message, QnAMakerResults result)
+        {
+            var firstAnswer = result.Answers.First().Answer;
+            Activity answer = ((Activity)context.Activity).CreateReply();
+
+            var answerPipeData = firstAnswer.Split('|');
+
+            if (answerPipeData.Length == 1)
+            {
+                var answerData = firstAnswer.Split(';');
+                if (answerData.Length == 1)
+                {
+                    await context.PostAsync(firstAnswer);
+                    return;
+                }
+
+                AddHeroCardToAnswer(answer, answerData);
+            }
+            else
+            {
+                answer.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+
+                foreach (string itemAnswerData in answerPipeData)
+                {
+                    var answerData = itemAnswerData.Split(';');
+					if(answerData.Length >= 4)
+						AddHeroCardToAnswer(answer, answerData);
+                }
+            }
+
+            await context.PostAsync(answer);
+        }
+    }
+}
